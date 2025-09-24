62f957b2e831517878316673bbe8ccd2d2f1daa1
VladislavVenin <d4spda@yandex.ru>
2025-09-24 23:23:56 +1100

    Update utils.py

--- datacenter/utils.py
+++ datacenter/utils.py
@@ -5,28 +5,22 @@ def get_duration(visit):
     if visit.leaved_at is None:
         moscow_time = localtime()
         duration = moscow_time - visit.entered_at
-        return duration.seconds
     else:
         duration = visit.leaved_at - visit.entered_at
-        return duration.seconds
+    return duration.total_seconds()
 
 
 def format_duration(duration):
-    hours = duration//3600
-    minutes = duration % 3600 // 60
+    hour = 3600
+    minute = 60
+    hours = duration//hour
+    minutes = duration % hour // minute
     formated = f"{hours}ч, {minutes}мин"
     return formated
 
 
-def is_visit_long(visit, minutes=60):
-    if visit.leaved_at is not None:
-        duration = visit.leaved_at - visit.entered_at
-        duration = duration.total_seconds()/60
-        if duration > minutes:
-            return True
-    else:
-        duration = get_duration()/60
-        if duration > minutes:
-            return True
-        return False
+def is_visit_long(duration, long_visit=60):
+    minute = 60
+    if duration/minute > long_visit:
+        return True
     return False
